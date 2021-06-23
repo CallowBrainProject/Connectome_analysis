c6c77d92f702e145b7138642f485d418bdb3a05d
Daniel Callow <33704076+CallowBrainProject@users.noreply.github.com>
2021-06-23 12:20:12 -0400

    Create NODDI_estimation.sh

---
+++ preprocessing/NODDI_estimation.sh
@@ -0,0 +1,25 @@
+set echo
+set subj= ($argv[1]) \
+
+cd /data/bswift-1/dcallow/CONNECTOME/dwi/${subj}/T1w/Diffusion
+
+# Python
+python <<EOF
+
+# Set up amico 
+import amico
+import spams
+amico.core.setup()
+
+# Run Amico for NODDI estimation see https://github.com/daducci/AMICO/wiki/Fitting-the-NODDI-model for specific details
+ae = amico.Evaluation("/data/bswift-1/dcallow/CONNECTOME/dwi/", "${subj}/T1w/Diffusion")
+amico.util.fsl2scheme("/data/bswift-1/dcallow/CONNECTOME/dwi/${subj}/T1w/Diffusion/bvals","/data/bswift-1/dcallow/CONNECTOME/dwi/${subj}/T1w/Diffusion/bvecs",bStep=(0,$ae.load_data(dwi_filename = "data.nii.gz", scheme_filename = "bvals.scheme", mask_filename ="nodif_brain_mask.nii.gz", b0_thr = 5)
+ae.set_model("NODDI")
+ae.generate_kernels(regenerate=True)
+ae.load_kernels()
+ae.fit()
+
+# Save results
+ae.save_results()
+
+EOF
